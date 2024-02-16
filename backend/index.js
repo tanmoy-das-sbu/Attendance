833329e496ff16b1d1adb30a85b8ed54c7670fa3
tanmoy-das-sbu <147819968+tanmoy-das-sbu@users.noreply.github.com>
2024-02-16 16:42:02 +0530

    2

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,14 @@
 import dotenv from "dotenv";
-import connectDB from "./src/db/index.js";
+import connectDB from "./src/db/connectDB.js";
 import { app } from "./src/app.js";
 import cors from "cors"
 import bodyParser from "body-parser";
 import Room from "./src/routes/room.route.js"
-
-
 dotenv.config({
   path: "./env",
 });
-
 app.use(cors());
 app.use(bodyParser.json());
-
-
 connectDB()
   .then(() => {
     app.use('/rooms', Room);
@@ -26,23 +21,4 @@ connectDB()
     console.log("MONGODB connection FAILED !!!", err);
   });
 
-/*
-import express from "express"
-const app = express()
 
-(async () => {
-    try {
-       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-       app.on("Error:",(error)=>{
-        console.log("Error:", error);
-        throw error
-       })
-       app.listen(process.env.PORT,()=>{
-        console.log(`App is listening on port ${process.env.PORT}`);
-       })
-    } catch (error) {
-        console.error("Error:", error);
-        throw error
-    }
-})();
-*/
