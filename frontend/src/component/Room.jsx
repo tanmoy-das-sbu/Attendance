d632b689b4e5ab26d9e6113b43b68b6a5a24369e
tanmoy-das-sbu <147819968+tanmoy-das-sbu@users.noreply.github.com>
2024-02-17 16:31:36 +0530

    room search

--- frontend/src/component/Room.jsx
+++ frontend/src/component/Room.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import "./Room.css"
+import './Room.css';
+
 const Rooms = () => {
   const [rooms, setRooms] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     // Fetch room data from the backend
@@ -19,11 +21,26 @@ const Rooms = () => {
     fetchRooms();
   }, []);
 
+  // Filter rooms based on the search term
+  const filteredRooms = rooms.filter((room) =>
+    room.room.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="rooms-container">
       <h1 className="rooms-header">Room Tabs</h1>
+
+      {/* Search bar */}
+      <input
+        type="text"
+        placeholder="Search rooms"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="search-bar"
+      />
+
       <ul className="rooms-list">
-        {rooms.map((room) => (
+        {filteredRooms.map((room) => (
           <li key={room._id} className="room-item">
             <Link to={`/${room.room}`} className="room-link">
               {room.room}
